1748e1ac4050401fb5f568c6dba453608b113714
Ziga Stopinsek <sigi.kajzer@gmail.com>
2014-06-22 23:56:29 +0200

    added install script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ ! -f "deploy.sh" ]]; then
+	echo "Not in haki directory! cd to haki directory and try again!";
+fi;
+
+cp config.sh.template config.sh;
+
+mkdir -p "${HOME}/bin"
+ln -s "${HOME}/bin/haki" "${PWD}/deploy.sh"
+ln -s "${HOME}/bin/mkhakiproj" "${PWD}/mkproject.sh"
+
+echo "Create an environment variable HAKI_DIR";
