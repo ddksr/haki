0ee05607dc4dd5174fa4e84beb5998b7e080b8da
Ziga Stopinsek <sigi.kajzer@gmail.com>
2014-06-22 23:43:32 +0200

    default deploying environment and mkproject script created. should be ok for now

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,29 @@
+#!/bin/bash
 
+if [[ -z $HAKI_DIR ]]; then
+	echo "HAKI_DIR environment variable missing";
+	exit 1;
+fi;
+
+source "${HAKI_DIR}config.sh.template";
+
+PROJECT=$1;
+if [[ -z $PROJECT ]]; then
+	echo "No projects specified."
+	exit 1;
+fi;
+
+PROJECT_DIR="${HAKI_DIR}projects/${PROJECT}/";
+deploy_script="${PROJECT_DIR}deploy.sh";
+if [[ ! -f $deploy_script ]]; then
+	echo "Project $PROJECT does not exist."
+	exit 1;
+fi;
+
+shift;
+
+. $deploy_script $@;
+status=$?;
+
+
+exit $status;
--- mkproject.sh
+++ mkproject.sh
@@ -1,1 +1,27 @@
+#!/bin/bash
 
+if [[ -z $HAKI_DIR ]]; then
+	echo "HAKI_DIR environment variable missing";
+	exit 1;
+fi;
+
+source "${HAKI_DIR}config.sh.template";
+
+PROJECT=$1;
+if [[ -z $PROJECT ]]; then
+	echo "No projects specified."
+	exit 1;
+fi;
+
+project_dir="${HAKI_DIR}projects/${PROJECT}/";
+deploy_script="${project_dir}deploy.sh";
+if [[ -f $deploy_script ]]; then
+	echo "Project $PROJECT already exists!";
+	exit 1;
+fi;
+
+mkdir -p $project_dir &&
+echo "$DEFAULT_DEPLOY_SCRIPT_BODY" > $deploy_script &&
+chmod +x $deploy_script;
+
+exit 0;
